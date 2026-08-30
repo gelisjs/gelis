13fb7f0bf3eaf005553164763cb4b6015a8a569a
Rigent Tio Salma <rigentjogja@gmail.com>
2026-08-30 21:03:08 +0700

    refactor(types): enhance path validation in get and route methods

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import type { HttpMethod, RouteHandler, RouteRef } from "./route";
 
+import type { ValidRoutePath } from "./types/path";
+
 export class Gelis {
-  get<Path extends string>(
-    path: Path,
+  get<const Path extends string>(
+    path: Path & ValidRoutePath<Path>,
     handler: RouteHandler<Path>,
   ): RouteRef<"GET", Path> {
     void handler;
@@ -13,9 +15,9 @@ export class Gelis {
     };
   }
 
-  route<Method extends HttpMethod, Path extends string>(
+  route<const Method extends HttpMethod, const Path extends string>(
     method: Method,
-    path: Path,
+    path: Path & ValidRoutePath<Path>,
     handler: RouteHandler<Path>,
   ): RouteRef<Method, Path> {
     void handler;
